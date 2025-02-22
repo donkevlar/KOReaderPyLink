2032348fd5fdd70ad606276674208f63f2aae9b7
donkevlar <kevin.cayouette@gmail.com>
2025-02-22 14:40:13 -0500

    Added webhook capabilities

--- koreaderpylink.py
+++ koreaderpylink.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 import time
 import uuid
 import logging
@@ -6,9 +7,10 @@ from os import getenv
 from typing import Optional
 
 from dotenv import load_dotenv
+import httpx
 from fastapi import FastAPI, Header
 from fastapi.responses import JSONResponse, RedirectResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 from tinydb import Query, TinyDB
 
 app = FastAPI(openapi_url=None, redoc_url=None)
@@ -17,24 +19,50 @@ users = db.table("users")
 documents = db.table("documents")
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging = logging.getLogger("uvicorn")
 
 
 def str_to_bool(value: str) -> bool:
     return value.lower() in ("true", "1", "yes")
 
 
+# User Model
 class KosyncUser(BaseModel):
     username: Optional[str] = None
     password: Optional[str] = None
 
 
+# Document Model
 class KosyncDocument(BaseModel):
     document: Optional[str] = None
     progress: Optional[str] = None
     percentage: Optional[float] = None
     device: Optional[str] = None
     device_id: Optional[str] = None
+
+
+# Discord Payload Model
+class DiscordPayload(BaseModel):
+    content: Optional[str] = None
+    username: Optional[str] = None
+    avatar_url: Optional[str] = None
+    tts: Optional[bool] = False
+
+
+# Customizable webhook example: Send to discord webhook whenever a user connects to the service.
+async def send_webhook_discord(payload: DiscordPayload, webhook_url: str = os.getenv('WEBHOOK_URL'),
+                               webhook_enabled: bool = str_to_bool(os.getenv('WEBHOOK_ENABLED', 'False'))):
+    if webhook_enabled and webhook_url != '':
+        async with httpx.AsyncClient() as client:
+            response = await client.post(webhook_url, json=payload.model_dump())
+            logging.info(f'Sent Webhook to {webhook_url}, Status: {response.status_code}')
+            # If response has content, parse as JSON, otherwise return status only
+            try:
+                response_json = response.json() if response.content else None
+            except Exception as e:
+                response_json = None  # Handle cases where Discord returns non-JSON data
+
+            return {"status": response.status_code, "response": response_json}
 
 
 # redirects to healthstatus when no endpoint is used.
@@ -44,7 +72,7 @@ def root():
 
 
 @app.post("/users/create")
-def register(kosync_user: KosyncUser):
+async def register(kosync_user: KosyncUser):
     registrations_allowed = str_to_bool(getenv("OPEN_REGISTRATIONS", "True"))
     if registrations_allowed:
         if kosync_user.username is None or kosync_user.password is None:
@@ -54,6 +82,15 @@ def register(kosync_user: KosyncUser):
             return JSONResponse(status_code=409, content="Username is already registered.")
         if users.insert({'username': kosync_user.username, 'password': kosync_user.password}):
             logging.info(f"User {kosync_user.username} registered successfully.")
+            if str_to_bool(os.getenv('WEBHOOK_ENABLED', 'False')):
+                # Create Webhook Payload
+                payload = DiscordPayload()
+                payload.content = f"User **{kosync_user.username}** has successfully registered to KOReaderPyLink!"
+                payload.username = "KOReaderPyLink"
+                payload.avatar_url = 'https://donkevlar.github.io/KOReaderPyLink/icon/pylink.png'
+
+                response = await send_webhook_discord(payload=payload)
+
             return JSONResponse(status_code=201, content={"username": kosync_user.username})
         return JSONResponse(status_code=500, content="Unknown server error")
     else:
